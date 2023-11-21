bdd91f2b36c0f49783188bf616f05b3a2ff4111c
vd0904 <vedantdwivedi094@gmail.com>
2023-11-21 21:41:46 +0530

    edited

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,27 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// const Footer = () => {
+//   return (
+//     <div className="footer">
+//       <h1 className="text-center">Made with ❤️ &copy; Techinfoyt</h1>
+//       <p className="text-center mt-3">
+//         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+//         <Link to="/policy">Privacy Policy</Link>
+//       </p>
+//     </div>
+//   );
+// };
+
+
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div>
+            <h1>Footer comp</h1>
+        </div>
+    );
+};
+
+export default Footer;
+
