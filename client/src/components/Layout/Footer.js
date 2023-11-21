3b415bc5360173ef7a5828f320fbf8e410be4857
vd0904 <vedantdwivedi094@gmail.com>
2023-11-21 22:33:54 +0530

    images done

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,27 +1,17 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// const Footer = () => {
-//   return (
-//     <div className="footer">
-//       <h1 className="text-center">Made with ❤️ &copy; Techinfoyt</h1>
-//       <p className="text-center mt-3">
-//         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
-//         <Link to="/policy">Privacy Policy</Link>
-//       </p>
-//     </div>
-//   );
-// };
-
-
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Footer = () => {
-    return(
-        <div>
-            <h1>Footer comp</h1>
-        </div>
-    );
+  return (
+    <div className="footer">
+      <h1 className="text-center">Made with ❤️ &copy; Ved</h1>
+      <p className="text-center mt-3">
+        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+        <Link to="/policy">Privacy Policy</Link>
+      </p>
+    </div>
+  );
 };
+
 
 export default Footer;
 
