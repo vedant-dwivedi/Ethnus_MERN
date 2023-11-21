d770dc6aa16645fd6abcbcae3a9793fe0212665f
vd0904 <vedantdwivedi094@gmail.com>
2023-11-21 22:03:19 +0530

    edited

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { NavLink} from "react-router-dom";
+import { NavLink, Link} from "react-router-dom";
+import { MdCarRental } from "react-icons/md";
+
 const Header = () => {
     return(
         <>
@@ -9,19 +11,22 @@ const Header = () => {
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <NavLink to="/" className="navbar-brand" href="#">Hidden brand</NavLink>
+      <Link to="/" className="navbar-brand"> <MdCarRental /> Car Rental App</Link>
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <NavLink to="/" className="nav-link active" aria-current="page" href="#">Home</NavLink>
+          <NavLink to="/" className="nav-link" >Home</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/register" className="nav-link" href="#">Register</NavLink>
+          <NavLink to="/category" className="nav-link" >Category</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/login" className="nav-link" href="#">Login</NavLink>
+          <NavLink to="/register" className="nav-link" >Register</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/cart" className="nav-link" href="#">Cart(0)</NavLink>
+          <NavLink to="/login" className="nav-link" >Login</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/cart" className="nav-link" >Cart(0)</NavLink>
         </li>
         {/* <li className="nav-item">
           <NavLink to="/" className="nav-link disabled" aria-disabled="true">Disabled</NavLink>
