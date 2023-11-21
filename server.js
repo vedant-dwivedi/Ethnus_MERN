3caa3f6b43e30b208edcf792d07e9215a5bb17c8
vd0904 <vedantdwivedi094@gmail.com>
2023-11-21 13:35:31 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from 'morgan'
+import connectDB from "./config/db.js";
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+// rest object
+const app = express()
+
+//middlewares
+app.use(express.json())
+
+// rest api
+app.get('/', (req,res) => {
+    res.send("<h1>Welcome to Car Rental MERN Stack Project</h1>");
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () =>{
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+} );
