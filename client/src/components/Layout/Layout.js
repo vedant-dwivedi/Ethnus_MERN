11cda2de5d4a1602ad632b0620b5a948c8a66b32
vd0904 <vedantdwivedi094@gmail.com>
2023-11-22 13:37:47 +0530

    done

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,15 +1,33 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { Helmet } from "react-helmet";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <div>
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+          <meta name="author" content={author} />
+        </div>
+
+        <title>{title}</title>
+      </Helmet>
       <Header />
       <main style={{ minHeight: "70vh" }}>{children}</main>
       <Footer />
     </div>
   );
 };
+
+Layout.defaultProps = {
+  title: 'Car Rental App - rent now',
+  description: 'mern stack project',
+  keywords: 'mern,react,node,mongodb',
+  author: 'Vedant Dwivedi',
+}
 
 export default Layout;
--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -3,8 +3,25 @@ import Layout from "./../components/Layout/Layout";
 
 const Policy = () => {
   return (
-    <Layout>
-      <h1>Policy</h1>
+    <Layout title={'Privacy Policy - Car Rental App'}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+        </div>
+      </div>
     </Layout>
   );
 };
